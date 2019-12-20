a96644d668052e7cc4f281a6126f2237057b5b28
Bert Peters <bert@bertptrs.nl>
2019-12-20 09:06:13 +0100

    Get input file and implementation as CLI arguments.

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,55 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <fstream>
 #include "common.hpp"
 #include "json_dto.hpp"
 
-static std::string read_all_input() {
+typedef std::string serializer_t(const SampleStruct&);
+typedef SampleStruct deserializer_t(std::string_view);
+
+static std::string read_all_input(std::istream& input) {
     std::string result;
     char buffer[4096];
-    while (std::cin) {
-        std::cin.read(buffer, sizeof(buffer));
+    while (input) {
+        input.read(buffer, sizeof(buffer));
 
-        result.append(buffer, std::cin.gcount());
+        result.append(buffer, input.gcount());
     }
 
     return result;
 }
 
-int main(int, const char **) {
+int main(int argc, const char ** argv) {
+    serializer_t *serializer;
+    deserializer_t *deserializer;
+
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " IMPLEMENTATION INPUT_FILE" << std::endl;
+        return 1;
+    }
+
+    std::string_view implementation_name = argv[1];
+
+    if (implementation_name == "json_dto") {
+        serializer = serialize_json_dto;
+        deserializer = deserialize_json_dto;
+    } else {
+        std::cerr << "Unknown implementation: " << implementation_name << std::endl;
+        return 1;
+    }
+
+    std::ifstream input(argv[2]);
+
+
     using Clock = std::chrono::high_resolution_clock;
     constexpr int iterations = 100000;
-    auto data = read_all_input();
+    auto data = read_all_input(input);
     auto start = Clock::now();
 
     for (int i = 0; i < iterations; ++i) {
-        const SampleStruct sample = deserialize_json_dto(data);
-        data = serialize_json_dto(sample);
+        const SampleStruct sample = deserializer(data);
+        data = serializer(sample);
     }
 
     const SampleStruct sample = deserialize_json_dto(data);
